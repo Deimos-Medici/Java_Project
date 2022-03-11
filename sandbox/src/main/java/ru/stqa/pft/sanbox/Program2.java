e10810754420f783252cc7d5914f430f110cae92
Deimos-Medici <joker.flut@gmail.com>
2022-03-11 21:24:23 +0300

    Инициализация через конструктор

--- sandbox/src/main/java/ru/stqa/pft/sanbox/Program2.java
+++ sandbox/src/main/java/ru/stqa/pft/sanbox/Program2.java
@@ -2,12 +2,8 @@ package ru.stqa.pft.sanbox;
 
 public class Program2 {
     public static void main(String[] args){
-        Point a = new Point();
-        a.x = 6;
-        a.y = 9;
-        Point f = new Point();
-        f.x = 2;
-        f.y = 6;
+        Point a = new Point(6, 9);
+        Point f = new Point(2, 6);
         System.out.println("Расстояние между точками " + Point.distance(a, f));
     }
 
