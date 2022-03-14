42d049f0ae5b70be365587dd76a22096e5cdc3b3
Deimos-Medici <joker.flut@gmail.com>
2022-03-15 01:03:46 +0300

    Сделала 4 теста на два метода

--- sandbox/src/test/java/ru/stqa/pft/sanbox/FirstPointTest.java
+++ sandbox/src/test/java/ru/stqa/pft/sanbox/FirstPointTest.java
@@ -9,6 +9,28 @@ public class FirstPointTest {
     public void testPoint(){
         Point h = new Point(6, 9);
         Point j = new Point(2, 6);
-        Assert.assertEquals(Point.distance(h, j), 5.0);
+        Assert.assertEquals(Point.distance(h, j), 5);
+    }
+
+    @Test
+    public void  testPoint2(){
+        Point d = new Point(4,1);
+        Point s = new Point(4,7);
+        Assert.assertEquals(d.distance(s), 6);
+    }
+
+    @Test
+    public void testPoint3(){
+        Point y = new Point(5, 6);
+        Point g = new Point(5, 8);
+        Assert.assertEquals(Point.distance(y, g), 2);
+    }
+
+    @Test
+    public void  testPoint4(){
+        Point u = new Point(10,6);
+        Point b = new Point(2,6);
+        Assert.assertEquals(u.distance(b), 8);
     }
 }
+
