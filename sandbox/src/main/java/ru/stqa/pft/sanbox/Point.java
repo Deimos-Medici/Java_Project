760379ad12e629846520eb74c9c51713d3d420cf
Deimos-Medici <joker.flut@gmail.com>
2022-03-15 00:11:37 +0300

    Создала второй метод

--- sandbox/src/main/java/ru/stqa/pft/sanbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sanbox/Point.java
@@ -9,8 +9,21 @@ public class Point {
         this.y = y;
     }
 
+    public double sum() {
+        return x + y;
+    }
+
+    public double distance(Point p) {
+        return Math.sqrt(Math.pow(this.x - p.x, 2) + Math.pow(this.y - p.y, 2));
+//        return Math.sqrt((this.x - p.x)*(this.x - p.x)+(this.y - p.y)*(this.y - p.y));
+    }
+
     public static double distance(Point p1, Point p2){
         return Math.sqrt((p2.x - p1.x)*(p2.x - p1.x)+(p2.y - p1.y)*(p2.y - p1.y));
+    }
+
+    public static double sum(Point p) {
+        return p.x +p.y;
     }
 
 }
--- sandbox/src/main/java/ru/stqa/pft/sanbox/Program2.java
+++ sandbox/src/main/java/ru/stqa/pft/sanbox/Program2.java
@@ -4,7 +4,9 @@ public class Program2 {
     public static void main(String[] args){
         Point a = new Point(6, 9);
         Point f = new Point(2, 6);
-        System.out.println("Расстояние между точками " + Point.distance(a, f));
+        System.out.println("Расстояние между точками первый метод " + Point.distance(a, f));
+
+        System.out.println("Расстояние между точками второй метод " + a.distance(f));
     }
 
 }
