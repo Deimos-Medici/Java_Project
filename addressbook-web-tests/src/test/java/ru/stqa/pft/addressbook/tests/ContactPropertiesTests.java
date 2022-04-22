8aa6b44a65f3a1d59d73bd8d50531f79bedb29f5
Deimos-Medici <joker.flut@gmail.com>
2022-04-22 16:38:51 +0300

    Объединила три теста контактов в один (номера, адреса, почты)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactPropertiesTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactPropertiesTests.java
@@ -9,16 +9,28 @@ import java.util.stream.Collectors;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
-public class ContactEmailTests extends TestBase{
+public class ContactPropertiesTests extends TestBase{
 
     @Test
-    public void testContactPhones(){
+    public void testContactProperties(){
         app.contact().Home();
         ContactData contact = app.contact().all().iterator().next();
         ContactData contactInfoFromEditForm = app.contact().infoFromEditForm(contact);
 
+        assertThat(contact.getAllPhones(), equalTo(mergePhones(contactInfoFromEditForm)));
+        assertThat(contact.getAddress(), equalTo(contactInfoFromEditForm.getAddress()));
         assertThat(contact.getAllMails(), equalTo(mergeMails(contactInfoFromEditForm)));
+    }
 
+    private <T> String mergePhones(ContactData contact) {
+        return Arrays.asList(contact.getHomePhone(), contact.getMobilePhone(), contact.getWorkPhone())
+                .stream().filter((s) -> ! s.equals(""))
+                .map(ContactPropertiesTests::cleaned)
+                .collect(Collectors.joining("\n"));
+    }
+
+    public static String cleaned(String phone){
+        return  phone.replaceAll("\\s","").replaceAll("[-()]","");
     }
 
     private <T> String mergeMails(ContactData contact) {
