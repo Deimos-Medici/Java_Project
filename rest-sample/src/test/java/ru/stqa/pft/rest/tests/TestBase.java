9513f832924f9887b17507775b3278637ddfc7d0
Deimos-Medici <joker.flut@gmail.com>
2022-06-21 20:17:19 +0300

    Проблема с java в интеграции

--- rest-sample/src/test/java/ru/stqa/pft/rest/tests/TestBase.java
+++ rest-sample/src/test/java/ru/stqa/pft/rest/tests/TestBase.java
@@ -2,34 +2,40 @@ package ru.stqa.pft.rest.tests;
 
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
+import com.jayway.restassured.RestAssured;
 import org.apache.http.client.fluent.Executor;
 import org.testng.SkipException;
+import org.testng.annotations.BeforeClass;
 
 import java.io.IOException;
 
 
 public class TestBase {
 
-
-
-    public Executor getExecutor() {
-        return Executor.newInstance().auth("288f44776e7bec4bf44fdfeb1e646490", "");
+    @BeforeClass
+    public void init(){
+        RestAssured.authentication = RestAssured.basic("288f44776e7bec4bf44fdfeb1e646490", "");
     }
 
-    //  public boolean isIssueOpen(int issueId) throws IOException {
+      public boolean isIssueOpen(int issueId) throws IOException {
 
-    //    JsonElement parsed = new  JsonParser().parse(json);
-      //  return parsed.getAsJsonObject().get("issues").getAsJsonObject().get(0).getAsJsonObject().get("state_name").getAsString();;
-      //  IssueData issue = mc.mc_issue_get(app.getProperty("web.adminLogin"), app.getProperty("web.adminPassword"), BigInteger.valueOf(issueId));
-      //  return !issue.getStatus().getName().equals("resolved");
-   // }
+        if (bagReportsCondition(issueId).equals("Open")){
+            return true;
+          }
+        return false;
+    }
 
+    private String bagReportsCondition(int issueId) {
+        String json = RestAssured.get("https://bugify.stqa.ru/api/issues.json").asString();
+        JsonElement parsed = new JsonParser().parse(json);
+        String issues = parsed.getAsJsonObject().get("issues").getAsJsonArray().get(0).getAsJsonObject().get("state_name").getAsString();
+        return issues;
+    }
 
-
-  //  public void skipIfNotFixed(int issueId) throws IOException {
-       // if (isIssueOpen(issueId)) {
-     //       throw new SkipException("Ignored because of issue " + issueId);
-     //   }
-  //  }
+    public void skipIfNotFixed(int issueId) throws IOException {
+        if (isIssueOpen(issueId)) {
+            throw new SkipException("Ignored because of issue " + issueId);
+        }
+    }
 
 }
